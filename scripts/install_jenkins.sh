1dbb48d6c16d7cdf946d1ae0fc3d0899493e7036
a7medhanafy <49996570+a7medhanafy@users.noreply.github.com>
2020-09-21 21:15:13 +0400

    Update install_jenkins.sh

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -15,6 +15,7 @@ usermod -aG docker ubuntu
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
 docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
+docker update --restart=always jenkins
 
 # show endpoint
 echo 'Jenkins installed'
